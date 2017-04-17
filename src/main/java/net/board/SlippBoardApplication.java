26c9479de86968f2f9e96272145c4325afbdd543
JungJinSu <alive513@naver.com>
2017-04-17 10:36:49 +0900

    initial commit

---
+++ src/main/java/net/board/SlippBoardApplication.java
@@ -0,0 +1,12 @@
+package net.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlippBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlippBoardApplication.class, args);
+	}
+}
