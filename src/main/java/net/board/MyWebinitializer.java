a72b4ebd4b98ba1c3a191054afb295e8c4a6d575
JungJinSu <alive513@naver.com>
2017-04-20 11:00:08 +0900

    4-6. (추가 내용)  - jar : 모바일과 같이 웹자원을 관리하지 않고 서버에서 데이터만 제공.   web 같은 경우 이미지, html 포함하는 경우 문제가 될 수 있다. 1) 패키지 설정 변경 : <packaging>war</packaging>
    2) 내장된 톰캣 대신 별도로 메이븐에서 설치하여 사용
     : <dependency>
    <groupId>org.springframework.boot</groupId>
    <artifactId>spring-boot-starter-tomcat</artifactId>
    <scope>provided</scope>
    </dependency>
    
     -  단,  내장 톰캣은 자동으로 초기화 되지만, 외장 톰캣의 경우 초기화클래스가 필요하다
    public class MyWebinitializer extends SpringBootServletInitializer {
    @Override
    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
     return builder.sources(SlippBoardApplication.class);
    }
    }
    
     - 톰캣 배포 및 구조 이해
    tomcat / webapps / ROOT 내의 코드가 배포되게 된다.
    배포전에 이전 내용을 지운뒤, 내 프로젝트를 톰캣에 저장 및 이름 변경
    rm -rf ROOT
    mv myProject  ~/tomcat/webapps
    mv myProject / ROOT
    
     - 하나의 톰캣 서버에 여러개의 프로젝트 실행이 가능하다.
       tomcat / ROOT, doc, example 등 하위
     - 웹자원을 포함하고 있는 경우 war 로 배포

--- src/main/java/net/board/MyWebinitializer.java
+++ src/main/java/net/board/MyWebinitializer.java
@@ -3,7 +3,7 @@ package net.board;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-// 내장 톰캣은 자동으로 초기화 되지만, 외장 톰캣의 경우 초기화가 필요하다
+// 내장 톰캣은 자동으로 초기화 되지만, 외장 톰캣의 경우 초기화가 필요하다.
 public class MyWebinitializer extends SpringBootServletInitializer {
 
 	@Override
