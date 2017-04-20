94418a393bec6c5096826c6927d50cc5bdfc40f4
JungJinSu <alive513@naver.com>
2017-04-20 10:33:11 +0900

    4-6 서버 배포 작업  - war 파일 변환  - 외장 톰캣 사용 및 초기화 클래스 적용

---
+++ src/main/java/net/board/MyWebinitializer.java
@@ -0,0 +1,14 @@
+package net.board;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+// 내장 톰캣은 자동으로 초기화 되지만, 외장 톰캣의 경우 초기화가 필요하다
+public class MyWebinitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SlippBoardApplication.class);		// 내 프로젝트를 초기화 설정
+	}
+	
+}
